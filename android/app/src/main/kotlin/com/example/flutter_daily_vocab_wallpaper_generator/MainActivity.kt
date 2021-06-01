1b583283d1567bed4c2cdeab529d8b66b35af73d
Rashid Wassan <rashidwassaan@gmail.com>
2021-06-01 21:57:02 +0500

    initial setup

---
+++ android/app/src/main/kotlin/com/example/flutter_daily_vocab_wallpaper_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_daily_vocab_wallpaper_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WallpaperApp());
+}
+
+class WallpaperApp extends StatefulWidget {
+  @override
+  _WallpaperAppState createState() => _WallpaperAppState();
+}
+
+class _WallpaperAppState extends State<WallpaperApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
