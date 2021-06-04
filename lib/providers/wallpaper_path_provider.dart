842017c1e720512d3e4327e9d5c653bcf28ff88e
Rashid Wassan <rashidwassaan@gmail.com>
2021-06-05 00:59:45 +0500

    added new wallpapers

--- lib/providers/wallpaper_path_provider.dart
+++ lib/providers/wallpaper_path_provider.dart
@@ -4,13 +4,13 @@ class WallpaperProvider with ChangeNotifier {
   int wallpaper_no = 1;
 
   void changeToNextWallpaper() {
-    if (wallpaper_no == 16) wallpaper_no = 0;
+    if (wallpaper_no == 14) wallpaper_no = 0;
     wallpaper_no = wallpaper_no + 1;
     notifyListeners();
   }
 
   void changeToPreviousWallpaper() {
-    if (wallpaper_no == 1) wallpaper_no = 16;
+    if (wallpaper_no == 1) wallpaper_no = 14;
     wallpaper_no = wallpaper_no - 1;
     notifyListeners();
   }
